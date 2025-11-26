5f9e31da41e2d501d173bd517ff9d3f56441a1c1
idsanche95 <ivanchodark@live.com>
2025-11-26 16:16:27 -0500

    siguiente commit

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github/ivanchodark/godesde0/variables"
 )
 
@@ -8,5 +9,10 @@ import (
 
 func main() {
 	//fmt.Println("Hola mundo!!")
-	variables.MostrarEnteros()
+	//variables.MostrarEnteros()
+	//mismo paquete, archivos diferentes
+	//variables.RestoVariables()
+	Estado, Texto := variables.ConviertoaTexto(1995)
+	fmt.Println(Estado)
+	fmt.Println(Texto)
 }
---
+++ variables/resto.go
@@ -0,0 +1,33 @@
+package variables
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
+
+// vaiable Global ya que inicia con mayuscula, para  el que importe este paquete
+var Nombre string
+
+// vaiable local ya que inicia con minuscula,
+var nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time // para variables con fechas hay que importar libreria time
+
+func RestoVariables() {
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 17.66
+	Fecha = time.Now()
+	println(Nombre)
+	println(Estado)
+	println(Sueldo)
+	fmt.Println(Fecha)
+}
+
+// (parm entrada)(valores de salida)
+func ConviertoaTexto(numeri int) (bool, string) {
+	texto := strconv.Itoa(numeri)
+	return true, texto
+}
