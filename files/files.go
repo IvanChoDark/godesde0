0db915503162037c2338a046bfeba01b28430a4c
idsanche95 <ivanchodark@live.com>
2025-12-17 10:31:24 -0500

    siguiente commit

--- files/files.go
+++ files/files.go
@@ -1,10 +1,9 @@
 package files
 
 import (
-	//"bufio"
+	"bufio"
 	"fmt"
 	"github/ivanchodark/godesde0/ejercicios"
-	//"ioutil"
 	"os"
 )
 
@@ -23,21 +22,36 @@ func GrabarTabla() {
 
 func SumaTabla() {
 	var texto string = ejercicios.TablaMultiplicar()
-	if Append(filename, texto) == false {
+	if !Append(filename, texto) {
 		fmt.Println("Error al concatenar contenido")
 	}
 }
 
 func Append(file string, texto string) bool {
 	arch, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil{
-		fmt.Println("Error durante el Append" + err.Error)
+	if err != nil {
+		fmt.Println("Error durante el Append" + err.Error())
 		return false
 	}
 	_, err = arch.WriteString(texto)
-	if err != nil{
-		fmt.Println("Error durante el WriteString" + err.Error)
+	if err != nil {
+		fmt.Println("Error durante el WriteString" + err.Error())
 		return false
 	}
 	arch.Close()
+	return true
+}
+
+func LeoArchivo() {
+	archivo, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("Error al leer el archivo" + err.Error())
+		return
+	}
+	scanner := bufio.NewScanner(archivo)
+	for scanner.Scan() {
+		registro := scanner.Text()
+		println("> " + registro)
+	}
+	archivo.Close()
 }
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	//"fmt"
 	//"github/ivanchodark/godesde0/ejercicios"
 	"github/ivanchodark/godesde0/files"
-	//"fmt"
 	//"github/ivanchodark/godesde0/teclado"
 	//"github/ivanchodark/godesde0/iteraciones"
 	//"runtime" // informacion del equipo en que se esta corriendo el app
@@ -14,34 +13,35 @@ import (
 //"fmt"
 
 func main() {
-	/*
-		//fmt.Println("Hola mundo!!")
-		//variables.MostrarEnteros()
-		//mismo paquete, archivos diferentes
-		//variables.RestoVariables()
-		Estado, Texto := variables.ConviertoaTexto(1995)
-		fmt.Println(Estado)
-		fmt.Println(Texto)
-		//os := runtime.GOOS
-		if os := runtime.GOOS; os == "Linux." || os == "OS X." {
-			fmt.Println("esto no es Windows, es: ", os)
-		} else {
-			fmt.Println("esto es Windows")
-		}
 
-		switch os := runtime.GOOS; os {
-		case "Linux. ":
-			fmt.Println("Esto es linux")
-		case "darwin":
-			fmt.Println("Esto es darwin")
-		default:
-			fmt.Printf("%s \n", os)
-		}
+	/*fmt.Println("Hola mundo!!") /*
+	//variables.MostrarEnteros()
+	//mismo paquete, archivos diferentes
+	//variables.RestoVariables()
+	Estado, Texto := variables.ConviertoaTexto(1995)
+	fmt.Println(Estado)
+	fmt.Println(Texto)
+	//os := runtime.GOOS
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("esto no es Windows, es: ", os)
+	} else {
+		fmt.Println("esto es Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "Linux. ":
+		fmt.Println("Esto es linux")
+	case "darwin":
+		fmt.Println("Esto es darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 	*/
 	//fmt.Println(ejercicios.TestValor("2000"))
 	//teclado.IngresoNumeros()
 	//iteraciones.Iterar()
 	//fmt.Println(ejercicios.TablaMultiplicar())
-	files.GrabarTabla()
+	//files.GrabarTabla()
 	//files.SumaTabla()
+	files.LeoArchivo()
 }
